a8b4c1d7f7f4c288f08414d5bdc0f5084243981e
KathySega <ari.sega@gmail.com>
2021-12-28 23:57:28 -0600

    adding test file for opencart

---
+++ hello-world/cypress/integration/test/opencartTest.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+
+//Scenarios to test:
+
+describe('Opencart Functional Test', () => {
+    beforeEach(() => {
+        cy.visit('http://opencart.abstracta.us/index.php')
+    })
+
+
+    //Functional test:
+    //1-Add items to a cart
+
+    //2-search items
+    //3-delete items from cart
+    //4-register flow
+    //5-login flow
+    //6-E2E flow
+
+    //Content scenarios:
+    //1-Validate ctas
+    //2-validate images
+    //3-validate dom
+
+
+
+
+})
